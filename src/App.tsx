9fabf1aa06d28beced851e671cff5a5c7c9ce799
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2022-03-07 18:52:02 -0300

    02. Componentizaçao

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Formulario from './components/Formulario'
+import Lista from './components/Lista'
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Formulario />
+      <Lista />
     </div>
   );
 }
